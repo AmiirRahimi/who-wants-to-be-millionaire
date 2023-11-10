afd6466a01fa2800715c27f0b0e15231bd745c5f
amir-rhmi <PKim@genedx.com>
2023-11-10 14:42:36 +0330

    push project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionsComponent } from './pages/questions/questions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
